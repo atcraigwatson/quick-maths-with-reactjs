cb60fa7a79c0aadefa551288c4ae99204c7743e4
Craig Waton <craig@watsonmemorials.co.uk>
2024-07-19 14:13:22 +0100

    Import Context to ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,8 +1,9 @@
 
 import { useContext } from 'react'
+import MathContext    from '../context'
 
 export default function ListItem() {
-  const num = useContext(MathContext);
+  const { num } = useContext(MathContext);
   return (
       <li className="list-group-item bg-dark-green text-white">1 X {num} = {num * 1}</li>
   );
