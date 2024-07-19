c5605da4138692db1af23179b4b6002cb6b0b8f5
Craig Waton <craig@watsonmemorials.co.uk>
2024-07-19 19:55:40 +0100

    Created Loop for List Items

--- src/components/MathCardNumTimesTableItem.tsx
+++ src/components/MathCardNumTimesTableItem.tsx
@@ -1,10 +1,29 @@
 
-import ListItem from './ListItem'
+import { useContext } from 'react'
+import MathContext    from '../context'
+import { JSX } from 'react/jsx-runtime';
 
 export default function MathCardNumTimesTableItem() {
-	return (
-		<ul className="list-group">
-			<ListItem />
-		</ul>
-	); 
+
+  const { num } = useContext(MathContext);
+  const timesTable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let timesTableList: JSX.Element[] = [];
+  
+  timesTable.forEach((times, index) => {
+    timesTableList.push(<li key={index} className="list-group-item bg-dark-green text-white">{times} X {num} = {num * times}</li>);
+  });
+  
+  if ( num <= 0 ) {
+    return ( 
+      <ul className="list-group">
+        <li className="list-group-item bg-dark-green text-white">We like to stay positve.. Please choose a number higher than 0.</li>
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="list-group">
+        {timesTableList}
+      </ul>
+    ); 
+  }
 };
